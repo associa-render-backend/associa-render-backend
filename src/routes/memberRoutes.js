8f7960b141be816dbb38034d2daf74caf36a9141
associa-render-backend <associa.financeapp@gmail.com>
2026-09-13 09:15:18 -0800

    Update memberRoutes.js

--- src/routes/memberRoutes.js
+++ src/routes/memberRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { sql } =
-require('../config/database');
+const db = require('../db');
 
 const authMiddleware =
 require('../middleware/authMiddleware');
